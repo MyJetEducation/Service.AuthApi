af5939f169864a97cceb60cd23a7b9decfddc9bc
dmitriy.b <dmitriy.b@smplt.net>
2022-02-25 11:16:00 +0500

    Refactoring: move info to token

--- src/Service.AuthApi/Modules/ServiceModule.cs
+++ src/Service.AuthApi/Modules/ServiceModule.cs
@@ -1,6 +1,7 @@
 ﻿using Autofac;
 using Microsoft.Extensions.Logging;
 using Service.Authorization.Client.Services;
+using Service.Core.Client.Services;
 using Service.Grpc;
 using Service.UserInfo.Crud.Client;
 using Service.UserInfo.Crud.Grpc;
@@ -11,16 +12,25 @@ namespace Service.AuthApi.Modules
 	{
 		protected override void Load(ContainerBuilder builder)
 		{
-			builder.RegisterUserInfoCrudClient(Program.Settings.UserInfoCrudServiceUrl, Program.LogFactory.CreateLogger(typeof(UserInfoCrudClientFactory)));
+			builder.RegisterUserInfoCrudClient(Program.Settings.UserInfoCrudServiceUrl, Program.LogFactory.CreateLogger(typeof (UserInfoCrudClientFactory)));
+			
+			builder.RegisterType<SystemClock>().AsImplementedInterfaces().SingleInstance();
+
+			builder.Register(context => new EncoderDecoder(Program.EncodingKey))
+				.As<IEncoderDecoder>()
+				.SingleInstance();
 
 			builder.Register(context =>
 				new TokenService(
 					context.Resolve<IGrpcServiceProxy<IUserInfoService>>(),
+					context.Resolve<IEncoderDecoder>(),
+					context.Resolve<ILogger<TokenService>>(),
+					context.Resolve<ISystemClock>(),
 					Program.Settings.JwtAudience,
 					Program.JwtSecret,
 					Program.Settings.JwtTokenExpireMinutes,
-					Program.Settings.RefreshTokenExpireMinutes,
-					context.Resolve<ILogger<TokenService>>()))
+					Program.Settings.RefreshTokenExpireMinutes
+					))
 				.As<ITokenService>()
 				.SingleInstance();
 		}
